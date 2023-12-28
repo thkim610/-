85012c727fd1724b7cc3836b3c1a11d80e821c59
thkim610 <thkim610@naver.com>
2023-12-28 11:16:25 +0900

    mod: BasicItemController 수정 - RedirectAttributes를 활용하여 status 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -55,7 +56,8 @@ public class BasicItemController {
     //상품 등록
     @PostMapping("/add")
     //@ModelAttribute의 name 속성을 생략했을 때 : 클래스명에서 첫글자만 소문자로 바꾼이름(Item -> item)으로 모델 객체를 만든다.
-    public String save(@ModelAttribute("item") Item item, Model model){
+    //RedirectAttributes : 리다이렉트와 관련된 속성을 넣는 인터페이스
+    public String save(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes){
         /*
         @ModelAttribute를 사용하면 아래의 모델 객체 생성 과정을 다 처리해준다.
          Item item = new Item();
@@ -64,14 +66,17 @@ public class BasicItemController {
          item.setQuantity(quantity);
         */
 
-        itemRepository.save(item);
+        Item savedItem = itemRepository.save(item);
         //model.addAttribute("item", item); //@ModelAttribute는 name 속성으로 지정한 이름("item")으로 모델에 객체를 저장한다.
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true); // 저장이 완료되었을 때 상태
 
         //return "basic/item"; //새로고침을 통해 바로 뷰 템플릿으로 넘어가면 계속 POST 호출이 되어 중복으로 등록되게 된다.
 
         //새로 고침 문제를 해결하기 위해 상품 저장 후에 상품 상세 화면으로 리다이렉트를 호출.(GET으로 재호출)
         /* PRG Post/Redirect/Get 해결 방식 */
-        return "redirect:/basic/items" + item.getId();
+        return "redirect:/basic/items/{itemId}";
     }
 
     //상품 수정 화면 출력
