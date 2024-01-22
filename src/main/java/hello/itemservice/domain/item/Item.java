8ddc2b71a5b3170d8fecc72d404ceef5a77faa06
thkim610 <thkim610@naver.com>
2024-01-22 14:14:56 +0900

    feat: Bean Validation 테스트 코드 작성

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,17 +1,34 @@
 package hello.itemservice.domain.item;
+/**
+ * javax.validation 으로 시작하면 특정 구현에 관계없이 제공되는 표준 인터페이스이고,
+ * org.hibernate.validator로 시작하면 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증 기
+ * 능이다.
+ */
 
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range; //hibernate.validator에서만 동작.
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank; //어떤 구현체에서도 동작.(표준)
+import javax.validation.constraints.NotNull; //어떤 구현체에서도 동작.(표준)
 import java.util.List;
 
 @Getter @Setter
 public class Item {
 
     private long id;
+
+    @NotBlank
     private String itemName; //상품명
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price; //상품가격, Integer를 쓴 이유 : null 값도 들어갈 수 있게 하기 위해서
+
+    @NotNull
+    @Max(9999)
     private Integer quantity; //수량
 
     private Boolean open; // 판매 여부 - HTML 폼에서 'on/off' 문자 값으로 넘어오는데 스프링은 이를 true/false로 변환해준다. (스프링 타입 컨버터)
---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,33 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        //item 검증을 위해 오류 값 세팅
+        Item item = new Item();
+        item.setItemName(" "); //공백
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        //검증 실행
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
+
+    }
+}
