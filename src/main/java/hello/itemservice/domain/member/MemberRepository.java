bf5710df356c6733a591429e1b4f4ddc4259e3f1
thkim610 <thkim610@naver.com>
2024-02-01 10:15:48 +0900

    mod: MemberRepository 기능 추가 - 회원조회, 회원전체조회, 로그인id로 회원 찾기

--- src/main/java/hello/itemservice/domain/member/MemberRepository.java
+++ src/main/java/hello/itemservice/domain/member/MemberRepository.java
@@ -4,8 +4,7 @@ import hello.itemservice.domain.member.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Repository
@@ -22,5 +21,28 @@ public class MemberRepository {
         store.put(member.getId(), member);
 
         return member;
+    }
+
+    //회원 조회
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    //회원 전체 조회
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values()); //Map에 저장된 member 객체들을 리스트에 저장하여 반환.
+    }
+
+    //로그인 id로 회원 찾기
+    public Optional<Member>findByLoginId(String loginId){
+        List<Member> all = new ArrayList<>();
+
+        return all.stream().filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    //초기화 메서드(테스트)
+    public void storeClear(){
+        store.clear();
     }
 }
